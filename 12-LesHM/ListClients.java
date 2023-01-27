7cc4198b2b8885cabf3b6aa3326cdc702ff8c2c6
Petr Bergman <petr.krg@mail.ru>
2023-01-27 14:51:04 +0600

    Правки по замечанию

--- 12-LesHM/ListClients.java
+++ 12-LesHM/ListClients.java
@@ -1,34 +1,53 @@
 package krg.petr.otusjava;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ListClients {
     private List<BankClient> listClients;
 
+    private Map<BankAccount, BankClient> clientByAccount;
+
     public ListClients() {
+
         this.listClients = new ArrayList<>();
+        this.clientByAccount = new HashMap<>();
+    }
+
+    public BankClient findClientByAccount(BankAccount account) {
+
+        return null;
     }
 
     public void add(BankClient client) {
+
         this.listClients.add(client);
     }
 
-    public void updateClient() {
-
-    }
-
-    public void deleteClient() {
-
-    }
-
-    public int getClientID(int elementIndex) {
-        return this.listClients.get(elementIndex).getClientID();
+    public void addToClientMap(BankAccount account, BankClient client) {
+        this.clientByAccount.put(account, client);
     }
 
     public BankClient getClient(int elementIndex) {
+
         return this.listClients.get(elementIndex);
+    }
+
+    public void showHashAccounts(String title, List<BankClient> fClient){
+
+        if ((this.listClients == null) || this.listClients.isEmpty()) {
+            return;
+        }
+
+        System.out.printf("%n%s%n", title);
+
+        String formatAlign = "| %-32s | %-4d |%n";
+
+        System.out.format("+----------------------------------+------+%n");
+        System.out.format("| Client name                      | Age  |%n");
+        System.out.format("+----------------------------------+------+%n");
+        System.out.format(formatAlign, fClient.get(0).getClientName(),
+                                       fClient.get(0).getClientAge());
+        System.out.format("+----------------------------------+------+%n%n");
     }
 
     public void showAllClients(String title) {
@@ -38,19 +57,19 @@ public class ListClients {
 
         System.out.printf("%n%s%n", title);
 
-        String formatAlign = "| %-4d | %-32s | %-4d |%n";
+        String formatAlign = "| %-32s | %-4d |%n";
 
-        System.out.format("+------+----------------------------------+------+%n");
-        System.out.format("| ID   | Client name                      | Age  |%n");
-        System.out.format("+------+----------------------------------+------+%n");
+        System.out.format("+----------------------------------+------+%n");
+        System.out.format("| Client name                      | Age  |%n");
+        System.out.format("+----------------------------------+------+%n");
 
         Iterator<BankClient> iterator = listClients.iterator();
         while (iterator.hasNext()) {
             BankClient item = iterator.next();
-            System.out.format(formatAlign, item.getClientID(), item.getClientName(), item.getClientAge());
+            System.out.format(formatAlign, item.getClientName(), item.getClientAge());
         }
 
-        System.out.format("+------+----------------------------------+------+%n%n");
+        System.out.format("+----------------------------------+------+%n%n");
     }
 
 }
--- 12-LesHM/helpers/HelperUtils.java
+++ 12-LesHM/helpers/HelperUtils.java
@@ -1,25 +1,14 @@
-package krg.petr.otusjava;
+package krg.petr.otusjava.helpers;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class HelperUtils {
-    private int lastClientID;
-    private int lastAccountID;
     private final String[] names = {"Дмитрий", "Максим", "Пётр", "Никита", "Артем", "Иван", "Кирилл", "Егор",
             "Илья", "Андрей"};
     private final String[] middleName = {"Николаевич", "Владимирович", "Александрович", "Иванович", "Васильевич",
             "Сергеевич", "Викторович", "Михайлович", "Артемович", "Петрович"};
     private final String[] surName = {"Иванов", "Смирнов", "Кузнецов", "Попов", "Васильев", "Петров", "Соколов",
             "Лазарев", "Медведев", "Ершов"};
-
-    public HelperUtils(int startClientID, int startAccountID) {
-
-        this.lastClientID = startClientID;
-        this.lastAccountID = startAccountID;
-    }
 
     public String generateRandomFIO() {
 
@@ -34,15 +23,6 @@ public class HelperUtils {
         final int MAX_INDEX = stringsArray.length;
         int randomIndex = new Random().nextInt(MAX_INDEX);
         return stringsArray[randomIndex];
-    }
-
-    public int generateClientID() {
-
-        return ++this.lastClientID;
-    }
-    public int generateAccountID() {
-
-        return ++this.lastAccountID;
     }
 
     public int generateBalance() {
